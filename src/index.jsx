eab9524fcc26a08fe772f0db301f6fd1cde4ca67
caleb-42 <ewisco316@yahoo.com>
2019-10-16 04:46:23 +0100

    ch-setup-css-framework
    - install sass dependencies for webpack bundling
    - install tailwind css framework
    - install eslint babel resolver

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "@styles/main.scss";
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return <h1 className="bg-teal-500">Hello</h1>;
 };
 
 ReactDOM.render(<App />, document.querySelector("#app"));
