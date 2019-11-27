d906c68ceec11baa4f40ea53a9e0d9a292b3eb47
caleb-42 <ewisco316@yahoo.com>
2019-11-27 04:52:12 +0100

    complete next integration

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export const axiosCall = ({
-  method='GET', path = '', payload = {}, cb =_=>{}
+  method = 'GET',
+  path = '',
+  payload = {},
+  cb = _ => {}
 }) => {
   const url = `/api/v1/${path}`;
   const headers = {
@@ -16,26 +19,34 @@ export const axiosCall = ({
   };
 
   return axios(axiosdata)
-  .then(result =>{
-    const data = result && result.data;
-    cb(data);
-    return data;
-  })
-  .catch (e=>{
-    cb(e);
-    let { error, message } = e;
-    throw error || message === 'Network Error' ? 'You are offline' : 'Something went wrong'
-  })
+    .then(result => {
+      const data = result && result.data;
+      cb(data);
+      console.error(data)
+      return data;
+    })
+    .catch(e => {
+      cb(e);
+      let {
+        error,
+        message
+      } = e; <<
+      << << < HEAD
+        ===
+        === =
+        console.error(e) >>>
+        >>> > complete next integration
+      throw error || message === 'Network Error' ? 'You are offline' : 'Something went wrong'
+    })
 };
 
 export const truncateText = (content, maxLength) => {
   if (content.length > maxLength) {
-    content = content.substr(0,maxLength) + '...';
+    content = content.substr(0, maxLength) + '...';
     return content;
   } else return content;
 }
 
 export const auto_grow = (e) => {
-  e.target.style.height = (e.target.scrollHeight)+"px";
+  e.target.style.height = (e.target.scrollHeight) + "px";
 }
-
