4b599406860d987cfe1242e1ec5a2b7376fd79c4
caleb-42 <ewisco316@yahoo.com>
2019-12-03 08:39:28 +0100

    Deploy gh-pages

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1575358231572");
+            injectCss(link, link.href.split("?")[0] + "?unix=1575358627366");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1575358230594");
+            injectCss(link, link.href.split("?")[0] + "?unix=1575358625867");
           });
       }
     }
@@ -103,7 +103,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1575358230613");
+            injectCss(link, link.href.split("?")[0] + "?unix=1575358625884");
           });
       }
     }
